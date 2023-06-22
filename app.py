7c4235ae5d1b32e5fb496773485a43b769572490
Gaumis <34369512+Gaumis@users.noreply.github.com>
2023-06-22 09:03:34 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # from flask module we are importing Flask class
-from flask import Flask
+from flask import Flask, render_template
 
 # object of Flask class is created with __name__ which is equal to __main__
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # @ symbol is decorator 
 @app.route("/")
 def hello_world():
-  return "Hello World"
+  return render_template('home.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
