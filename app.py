1e1906e6a62f6c8cc3870e529c1e07a166fd6278
Gaumis <34369512+Gaumis@users.noreply.github.com>
2023-06-26 16:25:25 +0000

    store applications in database

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # from flask module we are importing Flask class
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from sqlalchemy import text
-from database import engine, load_job_from_db
+from database import engine, load_job_from_db, add_application_to_db
 from filters import inr_format
 # object of Flask class is created with __name__ which is equal to __main__
 app = Flask(__name__)
@@ -22,7 +22,7 @@ def load_jobs_from_db():
 @app.route("/")
 def hello_world():
   all_jobs = load_jobs_from_db()
-  print(all_jobs)
+  #print(all_jobs)
   return render_template('home.html', jobs=all_jobs)
 
 
@@ -37,6 +37,20 @@ def load_specific_job(id):
   if not job:
     return "Not Found", 404
   return render_template('jobpage.html', job=job)
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  print(data)
+  add_application_to_db(id, data)
+  #store data in database
+  #send automated email to candidate
+  #display acknowledge mail to the candidate
+  return render_template('application_confirmation.html',
+                         application=data,
+                         job=job)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -15,3 +15,28 @@ def load_job_from_db(id):
     return dict(zip(result.keys(), job))
   else:
     return None
+
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    statement = text(
+      "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) values (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
+    )
+    result = conn.execute(
+      statement, {
+        "job_id": job_id,
+        "full_name": data['full_name'],
+        "email": data['email'],
+        "linkedin_url": data['linkedin_url'],
+        "education": data['education'],
+        "work_experience": data['work_experience'],
+        "resume_url": data['resume_url']
+      })
+
+def select_row_from_db(jod_id):
+  with engine.connect() as conn:
+    select_statement = text("SELECT * FROM applications WHERE job_id = :job_id")
+    select_result = conn.execute(select_statement, {"job_id": jod_id})
+    inserted_data = select_result.fetchone()
+    print("Inserted data:", inserted_data)
+
